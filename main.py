347c25f702924851789cd3245d4524f9ba2cdc09
thiagoquilici <thiagoquilici@gmail.com>
2024-06-03 20:59:42 -0300

    Versão 1.2.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # programa que executa cálculos geométricos
 
 # função
-def calcular_retangulo(base, altura):
-    area = base * altura
+def calcular_trapezio(base_maior, base_menor, altura):
+    area = base_maior + base_menor * altura / 2
     return area
 
 
@@ -20,12 +20,13 @@ def calcular_circulo(raio):
 
 
 # programa principal
-opcao = input('Escolha a figura (retângulo, triângulo ou círculo): ')
+opcao = input('Escolha a figura (trapézio, triângulo ou círculo): ')
 
-if opcao == 'retângulo':
-    base = float(input('Informe a base do retângulo: '))
-    altura = float(input('Informe a altura do retângulo: '))
-    print(f'Área do retângulo: {calcular_retangulo(base, altura)}')
+if opcao == 'trapézio':
+    base_maior = float(input('Informe a base maior do trapézio: '))
+    base_menor = float(input('Informe a base menor do trapézio: '))
+    altura = float(input('Informe a altura do trapézio: '))
+    print(f'Área do trapézio: {calcular_trapezio(base_maior, base_menor, altura)}')
 elif opcao == 'triângulo':
     base = float(input('Informe a base do triângulo: '))
     altura = float(input('Informe a altura do triângulo: '))
@@ -34,4 +35,4 @@ elif opcao == 'círculo':
     raio = float(input('Informe o raio do círculo: '))
     print(f'Área do círculo: {calcular_circulo(raio)}')
 else:
-    print('Opção inválida. Escolha entre retângulo, triângulo ou círculo.')
+    print('Opção inválida. Escolha entre trapézio, triângulo ou círculo.')
